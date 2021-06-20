cfe407b779dbbafa4a44727e2e4f2d6e7a9d5f17
masumi-soga <sg.kse777@gmail.com>
2021-06-20 18:08:32 +0900

    sample01変更

--- src/sample01.java
+++ src/sample01.java
@@ -1,19 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class sample01 {
 
   public static void main(String[] args) {
-
-    int count = 0;
-    int sum = 0;
-
-    for (int i = 1; i <= 10; i++) {
-      if(i == 1){
-        System.out.print(i);
-      }else{
-        System.out.print("," + i);
-      }
+    Integer[] array = new Integer[10];
+    for (int i = 0; i < array.length; i++) {
+      array[i] = i + 1;
     }
 
+    Arrays.sort(array, Collections.reverseOrder());
+    for (int j = 0; j < array.length; j++) {
+      if (j == 0) {
+        System.out.print(array[j]);
+      } else
+        System.out.print("," + array[j]);
+    }
 
   }
 }
