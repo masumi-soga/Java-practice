45cc6f38c30912fe1cddee7d6c0986b5b4a875fd
masumi-soga <sg.kse777@gmail.com>
2021-06-20 18:03:43 +0900

    JavaPractice

---
+++ src/sample01.java
@@ -0,0 +1,19 @@
+
+public class sample01 {
+
+  public static void main(String[] args) {
+
+    int count = 0;
+    int sum = 0;
+
+    for (int i = 1; i <= 10; i++) {
+      if(i == 1){
+        System.out.print(i);
+      }else{
+        System.out.print("," + i);
+      }
+    }
+
+
+  }
+}
